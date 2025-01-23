ebcf2c440ee85ae4ff6fc4da505a0f3b6d84a796
metanomad <0xnomad2082@gmail.com>
2025-01-23 22:42:11 +0800

    skill modify

--- src/settings.94422.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/loader/loader.fire",orientation:"",jsList:[],bundleVers:{app:"e0a39",internal:"d17e2",main:"b41b4"}};

405d0b23578bb19c7f83e479b22652a903e90819
metanomad <0xnomad2082@gmail.com>
2025-01-23 17:24:03 +0800

    alpha

---
+++ src/settings.94099.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/loader/loader.fire",orientation:"",jsList:[],bundleVers:{app:"6bf7d",internal:"d17e2",main:"b41b4"}};
